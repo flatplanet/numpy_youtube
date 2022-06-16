47500959295e38de1e69b0eb653da139fef5e3e9
John Elder <johne4196@gmail.com>
2022-06-16 09:11:10 -0700

    ufuncs

---
+++ fun.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# Numpy Universal Function
+np1 = np.array([-3,-2,-1,0,1,2,3,4,5,6,7,8,9])
+print(np1)
+
+# Square route of each element 
+#print(np.sqrt(np1))
+
+# Absolute Value
+#print(np.absolute(np1))
+
+# Exponents
+#print(np.exp(np1))
+
+# Min/Max
+#print(np.max(np1))
+#print(np.min(np1))
+
+# Sign positive or negative
+#print(np.sign(np1))
+
+# Trig sin cos log
+print(np.log(np1))
