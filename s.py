9a863c986c7d028f73c7dc130c4b167dd297b052
John Elder <johne4196@gmail.com>
2022-07-07 08:30:47 -0700

    sorting numpy arrays

---
+++ s.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# np.sort()  Numerical
+np1 = np.array([6,7,4,9,0,2,1])
+#print(np1)
+#print(np.sort(np1))
+
+
+# Alphabetical
+#np2 = np.array(["John", "Tina", "Aaron", "Zed"])
+#print(np2)
+#print(np.sort(np2))
+
+
+# Booleans T/F
+#np3 = np.array([True, False, False, True])
+#print(np3)
+#print(np.sort(np3))
+
+# Return a copy not change the original
+#print(np1)
+#print(np.sort(np1))
+#print(np1)
+
+# 2-d
+np4 = np.array([[6,7,1,9],[8,3,5,0]])
+print(np4)
+print(np.sort(np4))
